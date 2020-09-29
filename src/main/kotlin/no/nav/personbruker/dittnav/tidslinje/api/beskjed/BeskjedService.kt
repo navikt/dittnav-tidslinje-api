90f8fb4f35fa1bcd7cfc16f8241dbd9539e3f800
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2020-09-29 09:25:04 +0200

    Legger til Beskjed

---
+++ src/main/kotlin/no/nav/personbruker/dittnav/tidslinje/api/beskjed/BeskjedService.kt
@@ -0,0 +1,43 @@
+package no.nav.personbruker.dittnav.tidslinje.api.beskjed
+
+import no.nav.personbruker.dittnav.tidslinje.api.common.exception.ConsumeEventException
+import no.nav.personbruker.dittnav.tidslinje.api.common.InnloggetBruker
+
+class BeskjedService(private val beskjedConsumer: BeskjedConsumer) {
+
+    suspend fun getActiveBeskjedEvents(innloggetBruker: InnloggetBruker): List<BeskjedDTO> {
+        return getBeskjedEvents(innloggetBruker) {
+            beskjedConsumer.getExternalActiveEvents(it)
+        }
+    }
+
+    suspend fun getInactiveBeskjedEvents(innloggetBruker: InnloggetBruker): List<BeskjedDTO> {
+        return getBeskjedEvents(innloggetBruker) {
+            beskjedConsumer.getExternalInactiveEvents(it)
+        }
+    }
+
+    private suspend fun getBeskjedEvents(
+            innloggetBruker: InnloggetBruker,
+            getEvents: suspend (InnloggetBruker) -> List<Beskjed>
+    ): List<BeskjedDTO> {
+        return try {
+            val externalEvents = getEvents(innloggetBruker)
+            externalEvents.map { beskjed -> transformToDTO(beskjed, innloggetBruker) }
+        } catch(exception: Exception) {
+            throw ConsumeEventException("Klarte ikke hente eventer av type Beskjed", exception)
+        }
+    }
+
+    private fun transformToDTO(beskjed: Beskjed, innloggetBruker: InnloggetBruker): BeskjedDTO {
+        return if(innloggetBrukerIsAllowedToViewAllDataInEvent(beskjed, innloggetBruker)) {
+            toBeskjedDTO(beskjed)
+        } else {
+            toMaskedBeskjedDTO(beskjed)
+        }
+    }
+
+    private fun innloggetBrukerIsAllowedToViewAllDataInEvent(beskjed: Beskjed, innloggetBruker: InnloggetBruker): Boolean {
+        return innloggetBruker.innloggingsnivaa >= beskjed.sikkerhetsnivaa
+    }
+}
