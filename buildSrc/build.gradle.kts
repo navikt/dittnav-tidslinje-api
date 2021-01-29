d54c13cfc6c723be868c16b8371e5a87a86bf909
Christopher Santana <christopher.santana@nav.no>
2021-01-29 14:20:40 +0100

    Bumpet dittnav-dependencies.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2020.10.07-15.47-b9026ff285ab"
+val dittNavDependenciesVersion = "2021.01.28-13.10-57381c88aaab"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
